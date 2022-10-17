c0059a2920ec026b8850fe611c4a83fbd43d81ca
UBF21 <flashgames104@gmail.com>
2022-10-17 01:47:18 -0500

    diseño final,implementancion del binding,eventos y metodo para convertir las distintas monedas

--- src/app/componentes/convertidor/convertidor.component.ts
+++ src/app/componentes/convertidor/convertidor.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertidorComponent implements OnInit {
 
+  cantidad: number = 0;
+  tengo: string = 'USD';
+  quiero: string = 'EUR';
+  total: number = 0;
+
+  monedas: string[] = ['USD', 'EUR', 'LIBRA'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertir(): void {
+    switch (this.tengo) {
+      case 'USD':
+
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad;
+        } else if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 0.84;
+        } else if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.75;
+        } else {
+          console.log("Error");
+        }
+
+        break;
+      case 'EUR':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1.20;
+        } else if (this.quiero === 'EUR') {
+          this.total = this.cantidad;
+        } else if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.9;
+        } else {
+          console.log("Error");
+        }
+        break;
+      case 'LIBRA':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1.33;
+        } else if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 1.11;
+        } else if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad;
+        } else {
+          console.log("Error");
+        }
+        break;
+    }
   }
 
 }
