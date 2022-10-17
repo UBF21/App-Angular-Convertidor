0364997b2bac07e87a5a6746f9becb21ef2e59e4
UBF21 <flashgames104@gmail.com>
2022-10-16 19:08:41 -0500

    maquetando el proyecto de convertidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ConvertidorComponent } from './componentes/convertidor/convertidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConvertidorComponent
   ],
   imports: [
     BrowserModule,
